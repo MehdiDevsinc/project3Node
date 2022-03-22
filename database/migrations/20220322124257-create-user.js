d169ebeb83e5d0bbb1b3670fbe09d21f6c9f3df0
Dev <dev@macs-MacBook-Pro.local>
2022-03-22 19:13:45 +0500

    associations & db migrations done

---
+++ database/migrations/20220322124257-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      usename: {
+        type: Sequelize.STRING
+      },
+      useremail: {
+        type: Sequelize.STRING
+      },
+      userpassword: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ database/seeders/20220322131855-User.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+     return queryInterface.bulkInsert('Users', [{
+      usename: 'Mehdi',
+      useremail: 'mehdi@devsinc.com',
+      userpassword: '12345678',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     return queryInterface.bulkDelete('Users', null, {});
+  }
+};
